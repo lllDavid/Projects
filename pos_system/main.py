36f416b75baa2bb498a7c3c359743491f8f989f0
lllDavid <imperfectrising5527@gmail.com>
2024-11-11 16:23:43 +0100

    sync

--- pos_system/main.py
+++ pos_system/main.py
@@ -4,10 +4,16 @@ from datetime import datetime
 @dataclass
 class Main:
     app_name:str
+    app_version:str
+    is_running:bool
+    status:str
+
 
 def starting_app(app_name:str) -> None:
     print(f"{app_name} started application at: {datetime.now()}")
+    is_runnning = True
 
-starting_app(123)
+def get_products():
+    ...
 
 
