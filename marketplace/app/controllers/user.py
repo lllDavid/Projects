00b3e8d7e1f33634162a4e2461bc596b3ff45d12
lllDavid <imperfectrising5527@gmail.com>
2024-11-13 21:46:58 +0100

    sync

--- marketplace/app/controllers/user.py
+++ marketplace/app/controllers/user.py
@@ -1,17 +1,14 @@
-from models.user import User
-from models.user_db import User_DB
+from app.models.user import User
+from app.models.user_db import User_DB
 
 def register() -> None:
-    # Get input from the user
     username = str(input("Choose a username: "))
     password = str(input("Choose a password: "))
     email = str(input("Enter an e-mail address: "))
     reset_email = str(input("Enter a backup e-mail: "))
 
-    # Hash the password
     hashed_password = User.hash_password(password)
 
-    # Create the user object (this also returns the user)
     user = User.add_user(
         id=1,  
         ip_address="127.0.0.1",
@@ -28,14 +25,12 @@ def register() -> None:
         login_count=0,
         failed_login_attempts=0, 
     )
-    
-    # Once the user is created, add them to the database
     add_user_to_db(user)
 
 def add_user_to_db(user: User):
-    # Add the user to the database
-    User_DB.add_user(user)
+    user_db = User_DB()
+    user_db.add_user(user)
     print(f"User {user.username} added to the database!")
 
-if __name__ == "main":
+if __name__ == "__main__":
     register()
--- marketplace/app/models/user_db.py
+++ marketplace/app/models/user_db.py
@@ -1,11 +1,34 @@
-from dataclasses import dataclass
-from user import User
+import json
+from .user import User
+from datetime import datetime
 
-@dataclass
 class User_DB:
-    users: list[str] = []
+    users: list[User] = []
 
-    def add_user(self, user:User):
-        self.users.append(str(user))
+    def __init__(self):
+       # self.load_users()
+
+    def add_user(self, user: User):
+        self.users.append(user)
+        self.save_users()
         print(f"User {user.username} added to User DB")
 
+    def save_users(self):
+        users_data = []
+        for user in self.users:
+            user_data = user.__dict__.copy()
+            for key, value in user_data.items():
+                if isinstance(value, datetime):
+                    user_data[key] = value.isoformat()
+            users_data.append(user_data)
+
+        with open("users.json", "w") as f:
+            json.dump(users_data, f)
+
+    def load_users(self):
+        try:
+            with open("users.json", "r") as f:
+                users_data = json.load(f)
+                self.users = [User(**data) for data in users_data]
+        except FileNotFoundError:
+            self.users = []
