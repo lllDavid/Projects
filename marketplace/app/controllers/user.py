16e3a71ebf7b85e5c5c12e577293911208356ad4
lllDavid <imperfectrising5527@gmail.com>
2024-11-21 14:39:16 +0100

    sync

--- marketplace/app/controllers/user.py
+++ marketplace/app/controllers/user.py
@@ -2,17 +2,30 @@ from app.models.user import UserDetails, User, UserSecurity, UserStatus, UserLog
 from app.models.roles import Role
 from datetime import datetime
 
-def create_user(username: str, email: str, password: str):
-    user = User(id=1, username=username, email=email, password=password) 
+def get_user_input():
+    username = input("Username: ")
+    email = input("Email: ")
+    password = input("Password: ")
 
-    security = UserSecurity(
+    return {
+        "username": username,
+        "email": email,
+        "password": password
+    }
+
+def create_user(username: str, email: str, password: str) -> User:
+    return User(id=None, username=username, email=email, password=password)  
+
+def create_user_security(email: str, password: str) -> UserSecurity:
+    return UserSecurity(
         two_factor_enabled=False,
         password_hash=password,  
         reset_email=email,
         is_verified=False
     )
 
-    status = UserStatus(
+def create_user_status() -> UserStatus:
+    return UserStatus(
         is_banned=False,
         ban_reason="",
         is_active=True,
@@ -20,13 +33,20 @@ def create_user(username: str, email: str, password: str):
         updated_at=datetime.now()
     )
 
-    login_history = UserLoginHistory(
+def create_user_login_history() -> UserLoginHistory:
+    return UserLoginHistory(
         login_count=0,
         failed_login_attempts=0,
         last_login=datetime.now()
     )
 
-    user_details = UserDetails(
+def create_user_details(username: str, email: str, password: str) -> UserDetails:
+    user = create_user(username, email, password)
+    security = create_user_security(email, password)
+    status = create_user_status()
+    login_history = create_user_login_history()
+
+    return UserDetails(
         ip_address="",
         role=Role.USER,
         reset_email=email,
@@ -36,20 +56,15 @@ def create_user(username: str, email: str, password: str):
         login_history=login_history,
         user=user
     )
-    
-    print(f"Created user {username}")
+
+def create_and_save_user():
+    user_data = get_user_input()
+
+    user_details = create_user_details(user_data['username'], user_data['email'], user_data['password'])
+
+    print(f"Created user: {user_data['username']}")
     return user_details
 
-def get_user_input():
-    username = str(input("Username: "))
-    email =  str(input("Email: "))
-    password = str(input("Password: "))
-
-    return {
-        "username": username,
-        "email": email,
-        "password": password
-    }
 
 if __name__ == "__main__":
     user_input = get_user_input()
