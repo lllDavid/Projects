3d5863b983952ece6cbda356ff0ac0b933f9f8b4
lllDavid <imperfectrising5527@gmail.com>
2024-11-26 14:08:48 +0100

    sync

--- marketplace/app/misc/roles.py
+++ marketplace/app/misc/roles.py
@@ -2,5 +2,34 @@ from enum import Enum, auto
 
 class Role(Enum):
     USER = auto()
-    SUPPORT= auto()
+    SUPPORT = auto()
     ADMIN = auto()
+
+    def can_create_coin(self):
+        return self in {Role.ADMIN}
+
+    def can_edit_coin(self):
+        return self in {Role.ADMIN}
+
+    def can_delete_coin(self):
+        return self in {Role.ADMIN}
+    
+    def can_create_user(self):
+        return self in {Role.ADMIN}
+    
+    def can_edit_user(self):
+        return self in {Role.SUPPORT, Role.ADMIN}
+    
+    def can_delete_user(self):
+        return self in {Role.ADMIN}
+
+    def can_view(self):
+        return self in {Role.USER, Role.SUPPORT, Role.ADMIN}
+
+    def can_access_backup_codes(self):
+        # Only Admins can access or edit backup codes
+        return self == Role.ADMIN
+
+    def can_edit_backup_codes(self):
+        # Only Admins can edit backup codes
+        return self == Role.ADMIN
