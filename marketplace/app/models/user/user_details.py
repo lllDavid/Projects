d029663d95d064878dcd25c56507f41730b1ecc4
lllDavid <imperfectrising5527@gmail.com>
2024-12-01 13:45:08 +0100

    sync

--- marketplace/app/models/user/user_details.py
+++ marketplace/app/models/user/user_details.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from app.models.user import User, 
+from app.models.user.user import User
+from app.models.user.user_security import UserSecurity
+from app.models.user.user_status import UserStatus
+from app.models.user.user_login_history import UserLoginHistory
+from app.security.roles import Role
 @dataclass
 class UserDetails:
     user: User
