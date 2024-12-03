560cadd77c411539c6527fc738fdcc7c63cacba0
lllDavid <imperfectrising5527@gmail.com>
2024-12-03 14:52:15 +0100

    sync

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from marketplace.app.user.user_creator import user_creator_bp  
 
 def create_app() -> Flask:
     app = Flask(__name__, static_folder='app/static', template_folder='app/templates/')
 
-    @app.route('/home')
-    def home():
-        return render_template('home.html')
-    
     @app.route('/')
     def index():
         return redirect(url_for('home'))
     
+    @app.route('/landing')
+    def landing():
+        return render_template('landing.html')
+    
     @app.route('/signup')
     def signup():
-        return render_template('sign-up-form.html')
+        return render_template('signup.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+    
+    @app.route('/home')
+    def home():
+        return render_template('home.html')
 
     app.register_blueprint(user_creator_bp)
     app.config['SECRET_KEY'] = 'secret_key' 
