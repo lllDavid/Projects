08ec69e9c549a5ad6e4c78bd7315bfb1b45e2de1
lllDavid <imperfectrising5527@gmail.com>
2024-12-11 22:49:54 +0100

    sync

---
+++ marketplace/app/transaction/purchase.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from datetime import datetime
+from marketplace.app.user.user_deposit import UserDeposit
+
+@dataclass
+class Purchase:
+    user_id: str
+    user_desposit: UserDeposit
+    transaction_id: str
+    fiat_amount: float
+    coin_name: str
+    coin_amount: float
+    created_at: datetime
+    completed_at: datetime
+    status: str
+    successful: bool
---
+++ marketplace/app/transaction/trade.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+from marketplace.app.user.user_deposit import UserDeposit
+
+@dataclass
+class Trade:
+    user_id: int
+    user_desposit: UserDeposit
+    transaction_id: str
+    sender_address: str
+    receiver_address: str
+    coin_name: str
+    coin_amount: float
+    transaction_fee: float
+    created_at: datetime
+    completed_at: datetime
+    status: str
+    successful: bool
