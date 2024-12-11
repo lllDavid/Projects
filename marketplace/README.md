b3b811383e4d6d93d847ab9e47ba942680b1b628
lllDavid <imperfectrising5527@gmail.com>
2024-12-11 02:23:00 +0100

    sync

---
+++ marketplace/README.md
@@ -0,0 +1,30 @@
+# Crypto Marketplace
+This platform allows users to buy, sell, and exchange cryptocurrency in a secure and user-friendly environment. Built using Flask, Python, and a MariaDB database.
+
+## Features
+- Cryptocurrency buying, selling, and exchange functionality
+- Flask-based REST API
+- Integration with MariaDB for data storage
+- Real-time updates and transactions
+- Secure authentication (2FA via Authenticator App)
+
+---
+
+## Prerequisites
+
+Before running the application, ensure you have the following installed:
+
+- **Python 3.8+**: The application is built with Python.
+- **MariaDB**: The application uses MariaDB for database storage.
+- **Flask**: The application uses Flask as Webframework
+
+---
+
+## Setup Instructions
+
+Follow these steps to set up the environment and run the application.
+
+### 1. Clone the Repository
+
+git clone https://github.com/yourusername/crypto-marketplace.git
+cd crypto-marketplace
---
+++ marketplace/setup.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE marketplace;
+USE marketplace;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    role ENUM('User', 'Support', 'Admin') NOT NULL
+);
+
+CREATE TABLE user_security (
+	 id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT Null,
+    password_hash VARCHAR(255),
+    two_factor_enabled BOOLEAN,
+    two_factor_backup_codes_hash JSON,
+    two_factor_secret_key VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    is_banned BOOLEAN DEFAULT FALSE,
+    ban_reason TEXT,
+    ban_duration INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    login_count INT DEFAULT 0,
+    last_login TIMESTAMP,
+    failed_login_count INT DEFAULT 0,
+    last_failed_login TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+
