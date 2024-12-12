fbc0ad4c30b52132e7f5846d778cf53b1c6234a3
lllDavid <imperfectrising5527@gmail.com>
2024-12-12 23:14:03 +0100

    sync

--- marketplace/app/user/user_status.py
+++ marketplace/app/user/user_status.py
@@ -1,26 +1,56 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 
 @dataclass
 class UserStatus:
-    is_online: bool 
-    is_banned: bool 
+    is_online: bool
+    is_banned: bool
+    is_inactive: bool
     ban_reason: str | None = None
-    ban_duration: int | None = None    
+    ban_duration: int | None = None
+    ban_type: str | None = None  
+    ban_start_time: datetime | None = None  
+    ban_end_time: datetime | None = None  
 
-    def update_ban_status(self, is_banned: bool, reason: str, duration: int):
+    def update_ban_status(self, is_banned: bool, reason: str, ban_type: str, duration: int | None = None):
         self.is_banned = is_banned
         self.ban_reason = reason
-        self.ban_duration = duration
-        self.updated_at = datetime.now()
-        print(f"User banned: {is_banned}, Reason: {reason}, Duration: {duration}")
+        self.ban_type = ban_type
+        self.ban_start_time = datetime.now()
+
+        if ban_type == "temporary":
+            if duration is None:
+                raise ValueError("Duration must be specified for a temporary ban.")
+            self.ban_duration = duration
+            self.ban_end_time = self.ban_start_time + timedelta(days=duration)
+
+        elif ban_type == "permanent":
+            self.ban_duration = None
+            self.ban_end_time = None
+        else:
+            raise ValueError("Ban type must be 'temporary' or 'permanent'.")
+        
+        ban_duration_display = self.ban_duration if self.ban_type == 'temporary' else 'N/A'
+        print(f"User banned: {is_banned}, Reason: {reason}, Type: {ban_type}, Duration: {ban_duration_display}")
+
 
     def update_online_status(self, is_online: bool):
         self.is_online = is_online
         print(f"User Online: {is_online}.")
 
+    def update_inactivity_status(self, last_login: datetime | None, inactivity_threshold_days: int = 30):
+        if last_login is None:
+            self.is_inactive = True 
+        else:
+            if datetime.now() - last_login > timedelta(days=inactivity_threshold_days):
+                self.is_inactive = True
+            else:
+                self.is_inactive = False
+        print(f"User inactivity: {self.is_inactive}.")
+
     def __str__(self):
         return (f"Online: {self.is_online}, "
                 f"Banned: {self.is_banned}, "
                 f"Ban Reason: {self.ban_reason}, "
-                f"Ban Duration: {self.ban_duration}, ")
+                f"Ban Duration: {self.ban_duration}, "
+                f"Inactive: {self.is_inactive}")
