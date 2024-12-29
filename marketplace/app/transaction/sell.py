af1475cf5fafe8e5e7cc1164dfa83e0d4c95ae3c
lllDavid <imperfectrising5527@gmail.com>
2024-12-29 21:31:48 +0100

    sync

---
+++ marketplace/app/transaction/sell.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from marketplace.app.wallets.crypto.crypto_wallet import CryptoWallet
+from marketplace.app.wallets.fiat.fiat_wallet import FiatWallet
+
+@dataclass
+class Sell:
+    user_id: int
+    purchase_id: int
+    crypto_wallet: CryptoWallet
+    fiat_wallet: FiatWallet
+    transaction_id: str
+    fiat_amount: float
+    coin_name: str
+    coin_amount: float
+    created_at: datetime
+    completed_at: datetime
+    status: str
+    successful: bool
+    reference_number: int
+    
+
