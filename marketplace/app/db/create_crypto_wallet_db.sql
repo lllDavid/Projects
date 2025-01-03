474cde6c2e66641a24f8621dafdb883d0b5e6b9e
lllDavid <imperfectrising5527@gmail.com>
2025-01-03 15:46:18 +0100

    sync

--- marketplace/app/db/create_crypto_wallet_db.sql
+++ marketplace/app/db/create_crypto_wallet_db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE marketplace_wallets;
 USE marketplace_wallets;
 
-CREATE TABLE crypto_wallets (
+CREATE TABLE crypto_wallet (
     wallet_id INT PRIMARY KEY AUTO_INCREMENT,       -- Unique identifier for the wallet
     user_id INT,                                    -- Foreign key to link to the user (referencing `marketplace_users.id`)
     wallet_address VARCHAR(255),                     -- Wallet address (string)
@@ -11,5 +11,5 @@ CREATE TABLE crypto_wallets (
     encryption_key VARCHAR(255),                     -- Encryption key to secure wallet info
     deposit_history JSON DEFAULT '{}',               -- Deposit history stored as JSON for flexibility
     withdrawal_history JSON DEFAULT '{}',            -- Withdrawal history stored as JSON for flexibility
-    FOREIGN KEY (user_id) REFERENCES marketplace_users.id  -- Link to the `marketplace_users` table
+    FOREIGN KEY (user_id) REFERENCES marketplace_users.user(id)  -- Link to the `marketplace_users` table
 );
