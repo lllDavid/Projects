c9a32ac12cae0b9435d72734572a5ffb159b46e6
lllDavid <imperfectrising5527@gmail.com>
2025-01-03 15:54:59 +0100

    sync

--- marketplace/app/wallets/fiat/fiat_wallet_creator.py
+++ marketplace/app/wallets/fiat/fiat_wallet_creator.py
@@ -2,7 +2,6 @@ from marketplace.app.wallets.fiat.fiat_wallet import FiatWallet
 from marketplace.app.db.fiat_wallet_db import insert_fiat_wallet
 from marketplace.app.db.user_db import get_user_from_db
 
-
 def create_fiat_wallet(user_id: int) -> FiatWallet | None:
     user = get_user_from_db(user_id)
     if user is not None:
