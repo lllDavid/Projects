691d620fda4529ba725e1af6772cd93610316b82
lllDavid <imperfectrising5527@gmail.com>
2025-01-06 15:56:15 +0100

    sync

--- marketplace/app/views/crypto_liquidation.py
+++ marketplace/app/views/crypto_liquidation.py
@@ -1,12 +1,8 @@
-from decimal import Decimal
-from datetime import datetime
-
-from werkzeug.exceptions import BadRequest
 from flask import Blueprint, render_template, redirect, url_for, flash, session, request
 
-from marketplace.app.db.crypto_wallet_db import get_crypto_wallet_by_user_id, update_crypto_wallet
+from marketplace.app.db.crypto_wallet_db import get_crypto_wallet_by_user_id
 from marketplace.app.db.fiat_wallet_db import get_fiat_wallet_by_user_id
-
+from marketplace.app.transaction.liquidation import process_crypto_liquidation
 
 crypto_liquidation = Blueprint('crypto_liquidation', __name__)
 
@@ -20,42 +16,16 @@ def liquidate_crypto():
     if user_id is None:
         flash('You must be logged in to perform this action.', 'error')
         return redirect(url_for('login'))
-
-    wallet = get_crypto_wallet_by_user_id(user_id)
-    fiat_wallet = get_fiat_wallet_by_user_id(user_id)
-    print("Fiat Wallet BEFORE sell of Crypto:", fiat_wallet, "\n")
     
-    if wallet is not None:
-        try:
-            coin = request.form['coin-selection']
-            amount = request.form['coin-amount']
-            amount = Decimal(amount)
+    try:
+        fiat_wallet = get_fiat_wallet_by_user_id(user_id)
+        wallet = get_crypto_wallet_by_user_id(user_id)
 
-            if wallet.coins.get(coin, Decimal('0')) < amount:
-                flash('Insufficient coins in your crypto wallet', 'error')
-                return redirect(url_for('trade'))
+        success, message = process_crypto_liquidation(user_id, wallet, fiat_wallet, request.form)
 
-            wallet.remove_coins(coin, amount, datetime.now(), "")
-            wallet.add_withdrawal_to_history(datetime.now(), amount, method="crypto_withdraw")
-            update_crypto_wallet(wallet)
-            wallet.update_last_accessed()
-            # fiat_wallet.increase_wallet_balance()
+        flash(message[0], message[1])
+        return redirect(url_for('trade'))
 
-            curr_wallet = get_crypto_wallet_by_user_id(user_id)
-            print("Crypto wallet in DB AFTER sell: ", curr_wallet)
-            print("Crypto wallet AFTER sell: ",wallet)
-
-            flash(f'Successfully sold {amount} {coin}', 'success')
-            return redirect(url_for('trade'))
-
-        except BadRequest as e:
-            flash(f'Invalid data: {e}', 'error')
-            return redirect(url_for('trade'))
-
-        except Exception as e:
-            flash('An error occurred during the sale process. Please try again.', 'error')
-            return redirect(url_for('trade'))
-
-    else:
-        flash('No crypto wallet found for the user.', 'error')
+    except Exception as e:
+        flash('An error occurred during the sale process. Please try again.', 'error')
         return redirect(url_for('trade'))
