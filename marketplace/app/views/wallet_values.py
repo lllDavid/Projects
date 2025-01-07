98f385664cbe7d9b8dddfa1279ba935f7a234682
lllDavid <imperfectrising5527@gmail.com>
2025-01-07 14:14:58 +0100

    sync

--- marketplace/app/views/wallet_values.py
+++ marketplace/app/views/wallet_values.py
@@ -2,11 +2,16 @@ from flask import Blueprint, render_template, redirect, url_for, session
 from werkzeug.exceptions import BadRequest
 
 from marketplace.app.db.crypto_wallet_db import get_crypto_wallet_by_user_id
+from marketplace.app.controllers.auth_controller import check_authentication
 
 wallet_values = Blueprint('wallet_values', __name__)
 
 @wallet_values.route('/wallet', methods=['GET'])
 def create_wallet_form():
+    redirect_response = check_authentication()
+    if redirect_response:
+        return redirect_response
+
     return render_template('wallet.html')
 
 @wallet_values.route('/wallet', methods=['POST'])
@@ -14,10 +19,9 @@ def get_wallet_values():
     user_id = session.get('user_id')
     if user_id is None:
         return redirect(url_for('login'))
-    print(user_id)
     
     crypto_wallet = get_crypto_wallet_by_user_id(user_id)
-    
+
     return render_template('wallet.html', crypto_wallet=crypto_wallet)
 
     
