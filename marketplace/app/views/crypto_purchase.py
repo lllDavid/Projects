b26ff1adfae12df2882044803060c47a8d55b950
lllDavid <imperfectrising5527@gmail.com>
2025-01-07 14:14:58 +0100

    sync

--- marketplace/app/views/crypto_purchase.py
+++ marketplace/app/views/crypto_purchase.py
@@ -3,11 +3,15 @@ from flask import Blueprint, render_template, redirect, url_for, flash, session,
 from marketplace.app.db.crypto_wallet_db import get_crypto_wallet_by_user_id
 from marketplace.app.db.fiat_wallet_db import get_fiat_wallet_by_user_id
 from marketplace.app.transaction.purchase import process_crypto_purchase
+from marketplace.app.controllers.auth_controller import check_authentication
 
 crypto_purchase = Blueprint('crypto_purchase', __name__)
 
 @crypto_purchase.route('/trade', methods=['GET'])
 def create_trade_form():
+    redirect_response = check_authentication()
+    if redirect_response:
+        return redirect_response
     return render_template('trade.html')
 
 @crypto_purchase.route('/trade', methods=['POST'])
