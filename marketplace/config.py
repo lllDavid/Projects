9482754e9c6cef7c98541c29d9748f7422023eea
lllDavid <imperfectrising5527@gmail.com>
2025-01-10 14:29:55 +0100

    sync

--- marketplace/config.py
+++ marketplace/config.py
@@ -1,26 +1,24 @@
-from os import getenv
-
 class Config:
     USER_DB_CONFIG = {
-        "user": getenv("DB_USER", "user"),
-        "password": getenv("DB_PASSWORD", "user_password"),
-        "host": getenv("DB_HOST", "db"),  # Use the MariaDB container name as the host
-        "port": 3306,  # Ensure the correct port is used (3306 for MariaDB)
-        "database": getenv("DB_NAME", "crypto_app_db")
+        "user": "root",
+        "password": "root",
+        "host": "db",  
+        "port": 3306,  
+        "database": "marketplace_users"
     }
 
     COIN_DB_CONFIG = {
-        "user": getenv("DB_USER", "user"),
-        "password": getenv("DB_PASSWORD", "user_password"),
-        "host": getenv("DB_HOST", "db"),
+        "user": "root",
+        "password": "root",
+        "host": "db",
         "port": 3306,
-        "database": getenv("DB_NAME", "crypto_app_db")
+        "database": "marketplace_coins"
     }
 
     WALLET_DB_CONFIG = {
-        "user": getenv("DB_USER", "user"),
-        "password": getenv("DB_PASSWORD", "user_password"),
-        "host": getenv("DB_HOST", "db"),
+        "user": "root",
+        "password": "root",
+        "host": "db",
         "port": 3306,
-        "database": getenv("DB_NAME", "crypto_app_db")
+        "database": "marketplace_wallets"
     }
