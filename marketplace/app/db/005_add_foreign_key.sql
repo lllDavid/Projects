4ed0eea9d494df66e663eebf324532b1c0a7a924
lllDavid <imperfectrising5527@gmail.com>
2025-01-19 21:24:16 +0100

    sync

--- marketplace/app/db/005_add_foreign_key.sql
+++ marketplace/app/db/005_add_foreign_key.sql
@@ -1,12 +1,11 @@
-ALTER TABLE crypto_wallet
+ALTER TABLE marketplace_wallets.crypto_wallet
 ADD CONSTRAINT fk_user_id
 FOREIGN KEY (user_id)
-REFERENCES marketplace_users(id)
-ON DELETE CASCADE; 
+REFERENCES marketplace.marketplace_users(id)
+ON DELETE CASCADE;
 
-ALTER TABLE fiat_wallet
+ALTER TABLE marketplace_wallets.fiat_wallet
 ADD CONSTRAINT fk_user_id
 FOREIGN KEY (user_id)
-REFERENCES marketplace_users(id)
-ON DELETE CASCADE; 
-
+REFERENCES marketplace.marketplace_users(id)
+ON DELETE CASCADE;
