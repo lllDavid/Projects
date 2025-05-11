c2c7be65f51b778d93780a99d3c1b3ed3113e19b
lllDavid <imperfectrising5527@gmail.com>
2025-05-11 23:10:59 +0200

    sync

--- animal_classifier/classifier.py
+++ animal_classifier/classifier.py
@@ -1,22 +1,22 @@
-import os
-import torch
+from os import path
 from PIL import Image
+from model import Model
 from pathlib import Path
-from model import CNNModel
 from torchvision import transforms
+from torch import device, cuda, no_grad, load, sigmoid
 
 class Classifier:
     def main(self, image_path):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device_type = device("cuda" if cuda.is_available() else "cpu")
 
         input_shape = 128 * 128 * 3
         hidden_units = 1024
 
-        MODEL_PATH = Path("saved_models") / "CNNModel.pth"
-        loaded_model = CNNModel(input_shape=input_shape, hidden_units=hidden_units)
-        loaded_model.load_state_dict(torch.load(MODEL_PATH, map_location=device, weights_only=True))
+        MODEL_PATH = Path("saved_models") / "Model.pth"
+        loaded_model = Model(input_shape=input_shape, hidden_units=hidden_units)
+        loaded_model.load_state_dict(load(MODEL_PATH, map_location=device_type, weights_only=True))
         loaded_model.eval()
-        loaded_model.to(device)
+        loaded_model.to(device_type)
 
         transform = transforms.Compose([
             transforms.Resize((128, 128)),
@@ -26,14 +26,14 @@ class Classifier:
 
         def predict_image(image_path, model, transform):
             image = Image.open(image_path).convert('RGB')
-            image = transform(image).unsqueeze(0).to(device)
+            image = transform(image).unsqueeze(0).to(device_type)
 
-            with torch.no_grad():
+            with no_grad():
                 logits = model(image)
-                prediction = torch.sigmoid(logits).item() > 0.5 
+                prediction = sigmoid(logits).item() > 0.5 
             return prediction
         
         result = predict_image(image_path, loaded_model, transform)
 
-        resultString = f"The image {os.path.basename(image_path)} is a {'cat' if result else 'dog'}."
+        resultString = f"The image {path.basename(image_path)} is a {'cat' if result else 'dog'}."
         return resultString
