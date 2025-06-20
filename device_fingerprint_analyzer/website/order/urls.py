f1b44fb77dc020fb2e1181c74a6011336d48436b
lllDavid <imperfectrising5527@gmail.com>
2025-06-20 17:52:21 +0200

    sync

--- device_fingerprint_analyzer/website/order/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('checkout/', views.checkout, name='checkout'),
-]
--- device_fingerprint_analyzer/website/user/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import StoreUser
-
-admin.site.register(StoreUser)
